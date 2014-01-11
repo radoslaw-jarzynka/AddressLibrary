b52f36d280a453a7d6c5cc8b9cb29da87970a046
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2014-01-11 15:49:41 +0100

    pierwszy commit

---
+++ AddressLibrary/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressLibrary
+{
+    public class Address
+    {
+        public int subnet {get; private set; }
+        public int host {get; private set; }
+
+        public Address(int subnet, int host) {
+            this.subnet = subnet;
+            this.host = host;
+        }
+
+        public String toString() {
+            return subnet + "." + host;
+        }
+    }
+}
